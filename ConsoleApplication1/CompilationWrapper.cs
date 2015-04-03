04d190835a27af3411410060381e311e8a885903
Piotr Benetkiewicz <piotr@ais.pl>
2015-04-03 06:39:31 +0000

    initial commit

---
+++ ConsoleApplication1/CompilationWrapper.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp;
+using Microsoft.CodeAnalysis.Emit;
+using Microsoft.CSharp.RuntimeBinder;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    public class CompilationWrapper
+    {
+        public static byte[] Compile(params string[] sources)
+        {
+            var assemblyFileName = "testAsm";
+            var compilation = CSharpCompilation.Create(assemblyFileName,
+                options: new CSharpCompilationOptions(OutputKind.DynamicallyLinkedLibrary),
+                syntaxTrees: from source in sources
+                             select CSharpSyntaxTree.ParseText(source),
+                references: new[]
+        {
+            MetadataReference.CreateFromAssembly(typeof(object).Assembly)
+        });
+
+            EmitResult emitResult;
+
+            using (var ms = new MemoryStream())
+            {
+                emitResult = compilation.Emit(ms);
+
+                if (emitResult.Success)
+                {
+                    return ms.GetBuffer();
+                }
+            }
+
+            var message = string.Join("\r\n", emitResult.Diagnostics);
+            throw new ApplicationException(message);
+        }
+    }
+}
---
+++ ConsoleApplication1/Program.cs
@@ -0,0 +1,137 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp;
+using Microsoft.CodeAnalysis.CSharp.Symbols;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+using Microsoft.CodeAnalysis.Text;
+using Microsoft.CodeAnalysis.MSBuild;
+using System.Reflection;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static string source = @"using System;
+class Foo
+{
+    public static string Run()
+    {
+        return typeof(Foo).ToString();
+    }
+}";
+        static void Main(string[] args)
+        {
+            // SyntaxTree st = CSharpSyntaxTree.Create(SyntaxFactory.ClassDeclaration("Example").WithModifiers(SyntaxFactory.TokenList(SyntaxFactory.Token(SyntaxKind.PublicKeyword), SyntaxFactory.Token(SyntaxKind.StaticKeyword))).NormalizeWhitespace());
+            //Console.WriteLine(st.ToString());
+            // FindControllersWithWalker();
+            //FindControllersOfType();
+            //SyntaxTreeIncorrect();
+            AppDomainRun();
+        }
+
+        public static void AppDomainRun()
+        {
+            
+            byte[] assembly = CompilationWrapper.Compile(source);
+            //var type = assembly.GetType("Foo");
+            //var method = type.GetMethod("Run");
+            //var result = method.Invoke(null, new object[] { });
+
+            //Console.WriteLine("Hurray, the expression returned " + result);
+            var appDomain = AppDomain.CreateDomain(Guid.NewGuid().ToString());
+            appDomain.AssemblyResolve += AppDomain_AssemblyResolve;
+            appDomain.Load(assembly);
+            var foo = appDomain.CreateInstanceAndUnwrap("testAsm", "Foo");
+        }
+
+        private static Assembly AppDomain_AssemblyResolve(object sender, ResolveEventArgs args)
+        {
+            return Assembly.Load(CompilationWrapper.Compile(source));
+        }
+
+        public static void SyntaxTreeIncorrect()
+        {
+            var syntaxTree = CSharpSyntaxTree.ParseText("class Test { void Foo() { } int a }"); // TODO: contains diagnostics?
+            var root = syntaxTree.GetRoot();
+            Console.WriteLine(root);
+        }
+        public static void FindControllersWithWalker()
+        {
+            var workSpace = MSBuildWorkspace.Create();
+            var solution = workSpace.OpenSolutionAsync(@"C:\Users\piotratais\Documents\Visual Studio 2015\Projects\RoslynTarget\RoslynTarget.sln").Result;
+            foreach (var project in solution.Projects)
+            {
+                foreach (var document in project.Documents)
+                {
+                    var syntaxTree = document.GetSyntaxTreeAsync().Result;
+                    var semanticModel = document.GetSemanticModelAsync().Result;
+                    new ClassWalker(semanticModel).Visit(syntaxTree.GetRoot());
+                }
+            }
+                    
+        }
+
+        public static void FindControllersOfType()
+        {
+            var workSpace = MSBuildWorkspace.Create();
+            var solution = workSpace.OpenSolutionAsync(@"C:\Users\piotratais\Documents\Visual Studio 2015\Projects\RoslynTarget\RoslynTarget.sln").Result;
+            foreach (var project in solution.Projects)
+            {
+                foreach (var document in project.Documents)
+                {
+                    var syntaxTree = document.GetSyntaxTreeAsync().Result;
+                    var model = document.GetSemanticModelAsync().Result;
+                    var classSyntaxNodes = syntaxTree.GetRoot().DescendantNodes().OfType<ClassDeclarationSyntax>();
+                    foreach (var classSyntaxNode in classSyntaxNodes)
+                    {
+                        var classSymbol = model.GetDeclaredSymbol(classSyntaxNode);
+                        if (IsMvcController(classSymbol))
+                        {
+                            Console.WriteLine(classSymbol);
+                        }
+                    }
+                }
+            }
+        }
+
+        private static bool IsMvcController(INamedTypeSymbol x)
+        {
+            var classBaseType = x.BaseType;
+            if (classBaseType.ToString() == "object")
+            {
+                return false;
+            }
+
+            if (classBaseType.ToString() == "System.Web.Mvc.Controller")
+            {
+                return true;
+            }
+
+            return IsMvcController(classBaseType);
+        }
+
+        class ClassWalker : CSharpSyntaxWalker
+        {
+            private SemanticModel semanticModel;
+
+            public ClassWalker(SemanticModel semanticModel) 
+            {
+                this.semanticModel = semanticModel;
+            }
+
+            public override void VisitClassDeclaration(ClassDeclarationSyntax node)
+            {
+                var symbol = this.semanticModel.GetDeclaredSymbol(node);
+                if (IsMvcController(symbol))
+                {
+                    Console.WriteLine(symbol);
+                }
+
+                base.VisitClassDeclaration(node);
+            }
+        }
+    }
+}
